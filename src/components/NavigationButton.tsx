d7b17d5ccd2b12bc2384d5b4946d14c82d8c48e1
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-18 01:48:10 +0200

    feat(components): add styles to navigation component

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -13,10 +13,10 @@ const NavigationButton: React.FC<NavigationButtonProps> = ({
   disabled = false,
 }) => {
   return (
-    <Link href={path} passHref>
+    <Link href={path} passHref className="w-full">
       <button
         disabled={disabled}
-        className={`px-6 py-2 mt-4 bg-yellow-500 text-white font-bold rounded-lg shadow-lg hover:bg-yellow-600 transition duration-300 ${
+        className={`w-full px-6 py-2 mt-4 bg-yellow-500 text-white font-bold rounded-lg shadow-lg hover:bg-yellow-600 transition duration-300 ${
           disabled ? "opacity-50 cursor-not-allowed" : ""
         }`}
       >
