bc6c7210f14c2eb32411fb3f5769cf5f18c6dbe6
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-17 14:16:08 +0200

    feat(login): add login input to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,22 @@
 import { Inter } from "next/font/google";
+import { useCallback, useState } from "react";
+import LoginInput from "@/components/LoginInput";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const handleOnChange = useCallback((newUsername: string) => {
+    setUsername(newUsername);
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       Login page
+      <LoginInput onChange={handleOnChange} />
+      {username}
     </main>
   );
 }
