e60d0770bf000b9dfca1bece20f6c1a7dc43c13f
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-18 01:47:49 +0200

    feat(components): add style to error component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,9 +1,18 @@
-interface ErrorProps {
+type ErrorMessageProps = {
   message: string;
-}
+};
 
-const ErrorMessage: React.FC<ErrorProps> = ({ message }) => {
-  return <span>{message}</span>;
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+  if (!message) return null;
+
+  return (
+    <div
+      className="px-4 py-3 leading-normal text-red-700 bg-red-100 rounded-lg"
+      role="alert"
+    >
+      <span className="font-semibold">Error:</span> {message}
+    </div>
+  );
 };
 
 export default ErrorMessage;
