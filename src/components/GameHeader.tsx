e805a2b3cbae35f871144c8f79a88ebfbf7a58c0
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-18 01:00:42 +0200

    fix(game): import game config for client

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -7,12 +7,8 @@ export default function GameHeader(props: IGameHeader) {
   const { score, lifes } = props;
   return (
     <div className="w-full flex justify-between" style={{ height: "64px" }}>
-      <div className="text-3xl text-gray-900 dark:text-white">
-        Lifes: {lifes}
-      </div>
-      <div className="text-3xl text-gray-900 dark:text-white">
-        Score: {score}
-      </div>
+      <div className="text-3xl text-gray-900">Lifes: {lifes}</div>
+      <div className="text-3xl text-gray-900 ">Score: {score}</div>
     </div>
   );
 }
---
+++ src/config/game.ts
@@ -0,0 +1,7 @@
+export const config = {
+  game: {
+    DIFFICULTY_INCREASE: 0.05,
+    WAVE_ITEMS_NUMBER: 10,
+    WAVE_ITEMS_SPREAD: 0.2,
+  },
+};
