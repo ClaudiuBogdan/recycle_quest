a879214ddb37eb13e66a08893c54d00ac79d8c25
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-17 17:52:50 +0200

    feat(styles): change text color

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import LoginForm from "@/components/LoginForm";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Login() {
   const router = useRouter();
@@ -13,7 +10,7 @@ export default function Login() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       Login page
       <LoginForm onSuccess={handleRegisterSuccess} />
