00895bf2c5ec0623441610fe309ac25d7400f045
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-17 21:01:07 +0200

    chore(api): remove console log

--- src/adapters/firebase/init.ts
+++ src/adapters/firebase/init.ts
@@ -2,10 +2,7 @@
 import admin from "firebase-admin";
 import { config } from "@/config";
 
-console.log("Firebase credentials: ", config.firebase.credential);
 const firebaseCredential = JSON.parse(config.firebase.credential);
-console.log({ firebaseCredential });
-console.error("CREDENTIALS PRINTED");
 const firebaseConfig = {
   credential: admin.credential.cert(firebaseCredential),
   databaseURL: config.firebase.databaseURL,
