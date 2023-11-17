e88610084405a935c187f8d63cf1ba38bb67eb6e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-18 01:48:28 +0200

    feat(login): add style to login

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -28,6 +28,8 @@ const LoginForm: React.FC<LoginInputProps> = ({ onSuccess }) => {
   const submitText = "Enter";
   const loadingText = "Loading";
 
+  console.log("error: ", error);
+
   return (
     <div className={"space-y-4"}>
       <TextInput
@@ -41,7 +43,7 @@ const LoginForm: React.FC<LoginInputProps> = ({ onSuccess }) => {
         onClick={handleSubmitClick}
       />
       {loading && <Loading text={loadingText} />}
-      {error && <ErrorMessage message={error} />}
+      {error && <ErrorMessage message={error.message} />}
     </div>
   );
 };
