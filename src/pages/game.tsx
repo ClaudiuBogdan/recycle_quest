18d79e97131fba58382388c0fcc6acb3c90e3084
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-17 23:00:44 +0200

    feat(game): add game to game page

---
+++ src/pages/game.tsx
@@ -0,0 +1,31 @@
+import { GetServerSideProps } from "next";
+import { Inter } from "next/font/google";
+import { parseCookies } from "nookies";
+import Game from "./_game";
+
+const inter = Inter({ subsets: ["latin"] });
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+    >
+      <Game />
+    </main>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = parseCookies(context);
+  const userToken = cookies.token;
+
+  if (!userToken) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
