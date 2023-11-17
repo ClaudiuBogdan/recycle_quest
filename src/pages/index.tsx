c71c285c72aff0d6dfefb132f2c9d40bb3bf8a15
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-17 22:58:59 +0200

    feat(landing): add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
+import { GetServerSideProps } from "next";
 import { Inter } from "next/font/google";
-import Game from "./_game";
+import { parseCookies } from "nookies";
+import NavigationButton from "@/components/NavigationButton";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
+  const buttonStartText = "Let's play";
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <Game />
+      Landing page
+      <NavigationButton path={"/login"} buttonName={buttonStartText} />
     </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = parseCookies(context);
+  const userToken = cookies.token;
+
+  if (userToken) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
