11f1e006eec9b4b57286802eec2ee6e0e58301b4
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-19 19:52:12 +0200

    feat(game): mock trash item to test position

--- src/components/game/ConveryorBelt.tsx
+++ src/components/game/ConveryorBelt.tsx
@@ -1,16 +1,32 @@
+import { useCallback, useRef } from "react";
 import TrashItem from "./TrashItem";
+import usePosition from "./usePosition";
 
 type ConveyorBeltProps = {
   speed: number; // pixels per second
 };
 
 const ConveyorBelt: React.FC<ConveyorBeltProps> = ({ speed }) => {
+  const containerRef = useRef(null);
+  const position = usePosition(containerRef);
+
+  const handleItemOverflow = useCallback((id: number) => {
+    console.log("Handled overflow: ", id);
+  }, []);
+
+  const item = {
+    id: 1,
+    initialPosition: 500,
+  };
+
   return (
-    <div className="w-20 h-full bg-gray-600">
-      <TrashItem speed={speed} />
-      <TrashItem speed={speed} />
-      <TrashItem speed={speed} />
-      <TrashItem speed={speed} />
+    <div ref={containerRef} className="w-20 h-full bg-gray-600">
+      <TrashItem
+        item={item}
+        speed={speed}
+        containerPosition={position}
+        onOverflow={handleItemOverflow}
+      />
     </div>
   );
 };
