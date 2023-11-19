88b28f722c77a297ad86aa049b0ed6919f61e522
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-19 21:35:49 +0200

    feat(game): use items inside the game

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import ConveyorBelt from "./ConveryorBelt";
+import { useItems } from "./useItems";
 import useSpeed from "./useSpeed";
 
 interface GameProps {}
@@ -9,10 +10,14 @@ const Game: React.FC<GameProps> = () => {
   // Make the items to move in the conveyor belt
   // Remove the last item when the user click a container
   // Remove the last item when the item reaches the end of the conveyor belt
+  const { items, removeItem } = useItems();
   const speed = useSpeed();
+
+  const handleOverflow = removeItem;
+
   return (
     <div className="w-40 h-screen bg-yellow-200 relative overflow-hidden">
-      <ConveyorBelt speed={speed} />
+      <ConveyorBelt speed={speed} onOverflow={handleOverflow} items={items} />
     </div>
   );
 };
