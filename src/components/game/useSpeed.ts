c02dc816928a5f30d120c5a96581c6e25089ca26
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-19 23:35:31 +0200

    feat(game): update game speed

--- src/components/game/useSpeed.ts
+++ src/components/game/useSpeed.ts
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 
-const useSpeed = () => {
+const useSpeed = (gameEnded: boolean) => {
   const [speed, setSpeed] = useState(0);
-  const [factor, setFactor] = useState(10);
+  const [factor, setFactor] = useState(20);
 
   useEffect(() => {
+    if (gameEnded) {
+      setSpeed(0);
+      return;
+    }
     const devicePixelDensity =
       typeof window !== "undefined" ? window.devicePixelRatio || 1 : 1;
-    const newSpeed = devicePixelDensity * factor;
+    const newSpeed = factor / devicePixelDensity;
     setSpeed(newSpeed);
-
+    console.log(devicePixelDensity);
     const timeoutId = setTimeout(
       () => setFactor((factor) => (factor += 1)),
       1000,
     );
     return () => clearTimeout(timeoutId);
-  }, [factor]);
+  }, [factor, gameEnded]);
   return speed;
 };
 export default useSpeed;
