0b4e45f146f936c192e0bc656ef15ddbda787258
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-19 19:51:34 +0200

    feat(game): add overflow event to trash item

--- src/components/game/TrashItem.tsx
+++ src/components/game/TrashItem.tsx
@@ -1,32 +1,66 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { ITrashItem } from "./types";
+import { getPosition } from "./usePosition";
 
 interface TrashItemProps {
   // width: string;
+  item: ITrashItem;
   speed: number;
+  containerPosition: {
+    bottom: number;
+  } | null;
+  onOverflow: (id: number) => void;
 }
 
-const TrashItem: React.FC<TrashItemProps> = ({ speed }) => {
-  const itemRef = useRef<HTMLDivElement>(null);
+const TrashItem: React.FC<TrashItemProps> = ({
+  speed,
+  item,
+  containerPosition,
+  onOverflow,
+}) => {
+  const [overflowed, setOverflowed] = useState(false);
+  const divRef = useRef<HTMLDivElement>(null);
+  const itemRef = useRef(item);
   const lastTime = useRef(0);
 
   useEffect(() => {
+    if (overflowed) {
+      onOverflow(itemRef.current.id);
+    }
+  }, [overflowed, onOverflow]);
+
+  useEffect(() => {
     let animationId: number;
+    const item = itemRef.current;
     const animate = (timestamp: number) => {
+      if (!divRef.current) {
+        return;
+      }
       const currentTime = timestamp;
       const deltaTime = (currentTime - lastTime.current) / 1000;
-      if (deltaTime > 0) {
-        lastTime.current = currentTime;
+      const minMilSecPerFrame = 0.000016;
+      if (deltaTime < minMilSecPerFrame) {
+        return;
       }
+      lastTime.current = currentTime;
+      const currentY =
+        parseFloat(
+          divRef.current.style.transform
+            .replace("translateY(", "")
+            .replace("px)", ""),
+        ) || item.initialPosition;
+      const newY = currentY + speed * deltaTime;
+      divRef.current.style.transform = `translateY(${newY}px)`;
+      divRef.current.style.visibility = "visible";
 
-      if (itemRef.current) {
-        const currentY =
-          parseFloat(
-            itemRef.current.style.transform
-              .replace("translateY(", "")
-              .replace("px)", ""),
-          ) || 0;
-        const newY = currentY + speed * deltaTime;
-        itemRef.current.style.transform = `translateY(${newY}px)`;
+      // Check if the item has reached the end of the conveyor belt
+      const position = getPosition(divRef.current);
+      if (
+        position &&
+        containerPosition &&
+        position.bottom > containerPosition.bottom
+      ) {
+        setOverflowed(true);
       }
 
       animationId = requestAnimationFrame(animate);
@@ -34,9 +68,9 @@ const TrashItem: React.FC<TrashItemProps> = ({ speed }) => {
 
     animationId = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(animationId);
-  }, [speed]);
+  }, [speed, containerPosition]);
   return (
-    <div className="w-20 h-20 bg-white absolute top-0" ref={itemRef}>
+    <div className="w-20 h-20 bg-white absolute top-0 invisible" ref={divRef}>
       Trash
     </div>
   );
