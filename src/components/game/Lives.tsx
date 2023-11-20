631d8f8d36b3be1b3eb479a70978b30e8b38f851
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-20 21:39:07 +0200

    feat(game): update lives component style

--- src/components/game/Lives.tsx
+++ src/components/game/Lives.tsx
@@ -3,7 +3,14 @@ interface LivesProps {
 }
 
 const Lives: React.FC<LivesProps> = ({ count }) => {
-  return <div className="absolute top-0 right-10">Lives: {count}</div>;
+  return (
+    <div
+      className="absolute top-0 right-5 text-gray-800 font-extrabold shadow-lg p-5"
+      style={{ fontSize: "1rem" }}
+    >
+      Lives: {count}
+    </div>
+  );
 };
 
 export default Lives;
