8667531f5526e9b0c0b9f01fabde8585b31b86b5
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-21 12:02:32 +0200

    feat(api): create user auth check middleware

---
+++ src/pages/api/_guard.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextFunction, createMiddlewareDecorator } from "next-api-decorators";
+import { parseCookies } from "nookies";
+import { getUserByToken } from "@/adapters/firebase";
+
+const UserTokenGuard = createMiddlewareDecorator(
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
+    const { token } = parseCookies({ req });
+
+    if (!token) {
+      return res.status(401).json({ errors: [`Invalid auth token`] });
+    }
+
+    const user = await getUserByToken(token);
+    if (!user) {
+      return res.status(401).json({ errors: [`Unauthorized user`] });
+    }
+    next();
+  },
+);
+
+export { UserTokenGuard };
