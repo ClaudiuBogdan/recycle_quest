b5236ed59b12cfe41c76e6a4e8732ea24addccc4
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-23 22:15:37 +0200

    feat(score): add score service to calculate score based on events

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,12 +1,44 @@
 export interface GameData {
-  id: string;
+  id: string; // Unique identifier for the game session
   userId: string; // Reference to User
-  startTime: Date;
-  endTime: Date;
-  itemsSelected: Array<{
-    itemId: string;
-    binId: string;
-    correct: boolean;
-  }>;
-  score: number;
+  startTime: Date; // Start time of the game
+  endTime: Date; // End time of the game
+  events: Array<GameEvent>; // Array of game events
+  score: number; // Total score of the game
+}
+
+// Define a type for different game events
+export type GameEvent =
+  | ItemSelectedEvent
+  | ItemMissedEvent
+  | QuizItemEvent
+  | ScoreMultiplierEvent;
+
+// Define each event type
+export interface ItemSelectedEvent {
+  type: "itemSelected";
+  itemId: string; // ID of the item selected
+  binId: string; // ID of the bin selected
+  isCorrect: boolean; // Whether the selection was correct
+}
+
+export interface ItemMissedEvent {
+  type: "itemMissed"; // Event for missed items
+  itemId: string; // ID of the missed item
+  timestamp: Date;
+}
+
+export interface QuizItemEvent {
+  type: "quizItem";
+  questionId: string; // ID of the quiz question
+  answerId: string; // ID of the answer
+  isCorrect: boolean; // Whether the answer was correct
+  timestamp: Date;
+}
+
+export interface ScoreMultiplierEvent {
+  type: "scoreMultiplier"; // Type of multiplier event
+  active: boolean;
+  multiplier: number; // Multiplier value
+  timestamp: Date;
 }
---
+++ src/services/ScoreService.ts
@@ -0,0 +1,53 @@
+import { GameEvent } from "@/models/Game";
+
+const POINTS_FOR_CORRECT_SELECTION = 1;
+const POINTS_FOR_MISSED_ITEM = 1;
+const POINTS_FOR_QUIZ_CORRECT = 1;
+
+export type GameState = {
+  score: number;
+  multiplier: number;
+};
+
+export function calculateScore(
+  events: GameEvent | GameEvent[],
+  currentState?: GameState,
+): GameState {
+  const state: GameState = currentState
+    ? { ...currentState }
+    : { score: 0, multiplier: 1 };
+
+  if (!Array.isArray(events)) {
+    events = [events]; // Convert single event to an array for uniform processing
+  }
+
+  events.forEach((event) => {
+    switch (event.type) {
+      case "itemSelected":
+        // Score calculation for item selection
+        if (event.isCorrect) {
+          state.score += POINTS_FOR_CORRECT_SELECTION * state.multiplier;
+        }
+        break;
+      case "itemMissed":
+        // Deduct points or other logic for a missed item
+        state.score -= POINTS_FOR_MISSED_ITEM;
+        break;
+      case "quizItem":
+        // Score for quiz item
+        if (event.isCorrect) {
+          state.score += POINTS_FOR_QUIZ_CORRECT * state.multiplier;
+        }
+        break;
+      case "scoreMultiplier":
+        // Update multiplier
+        state.multiplier = event.active ? event.multiplier : 1;
+        break;
+      default:
+        // Handle other types or unexpected cases
+        break;
+    }
+  });
+
+  return state;
+}
