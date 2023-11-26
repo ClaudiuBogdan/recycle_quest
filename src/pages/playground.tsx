080ad3dd0f51d239575ccf2121182a2f104b7e07
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-26 18:06:20 +0200

    feat(tutorial): add tutorial page

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,9 +1,3 @@
-import TutorialComponent from "@/components/game/tutorial/Tutorial";
-
 export default function PlaygroundPage() {
-  return (
-    <main>
-      <TutorialComponent />
-    </main>
-  );
+  return <main></main>;
 }
