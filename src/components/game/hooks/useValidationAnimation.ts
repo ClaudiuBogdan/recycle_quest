bb3416033c40746091eb08a5ce2e388ad27a2770
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-07 17:57:47 +0200

    feat(game): use gradient for background color

--- src/components/game/hooks/useValidationAnimation.ts
+++ src/components/game/hooks/useValidationAnimation.ts
@@ -4,9 +4,9 @@ export function useValidationAnimation() {
   const [state, setState] = useState<ValidationState>("idle");
 
   const color = {
-    idle: "bg-yellow-200",
-    missed: "bg-red-400",
-    valid: "bg-green-400",
+    idle: "bg-gradient-to-r from-yellow-500 via-black-200 to-yellow-500",
+    missed: "bg-gradient-to-r from-red-500 via-black-200 to-red-500",
+    valid: "bg-gradient-to-r from-green-500 via-black-200 to-green-500",
   }[state];
 
   useEffect(() => {
