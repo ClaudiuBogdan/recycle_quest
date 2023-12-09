5da16e1e6eaabb22fd56a4876f3f50704b40bb9f
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-09 22:04:18 +0200

    remove(assets): remove initial assets

--- src/components/game/TrashItem/TrashItemImage.tsx
+++ src/components/game/TrashItem/TrashItemImage.tsx
@@ -1,9 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 
 import placeholderImage from "@/assets/placeholder64x64.png";
-import black1 from "@/assets/trashItems/black/black1.png";
-import black2 from "@/assets/trashItems/black/black2.png";
-import black3 from "@/assets/trashItems/black/black3.png";
 import rezidual_cheese from "@/assets/trashItems/black/rezidual_cheese.png";
 import rezidual_diaper from "@/assets/trashItems/black/rezidual_diaper.png";
 import rezidual_dirty_pizza_box from "@/assets/trashItems/black/rezidual_dirty_pizza_box.png";
@@ -11,26 +8,17 @@ import rezidual_eggs from "@/assets/trashItems/black/rezidual_eggs.png";
 import rezidual_fish_bones from "@/assets/trashItems/black/rezidual_fish_bones.png";
 import rezidual_mug from "@/assets/trashItems/black/rezidual_mug.png";
 import rezidual_used_napkin from "@/assets/trashItems/black/rezidual_used_napkin.png";
-import blue1 from "@/assets/trashItems/blue/blue1.png";
-import blue2 from "@/assets/trashItems/blue/blue2.png";
-import blue4 from "@/assets/trashItems/blue/blue3.png";
-import blue3 from "@/assets/trashItems/blue/blue3.png";
 import hartie_a4 from "@/assets/trashItems/blue/hartie_a4.png";
 import hartie_carton_cutie from "@/assets/trashItems/blue/hartie_carton_cutie.png";
 import hartie_carton_oua from "@/assets/trashItems/blue/hartie_carton_oua.png";
 import hartie_carton_pizza_clean from "@/assets/trashItems/blue/hartie_carton_pizza_clean.png";
 import hartie_punga from "@/assets/trashItems/blue/hartie_punga.png";
 import hartie_reviste from "@/assets/trashItems/blue/hartie_reviste.png";
-import brown1 from "@/assets/trashItems/brown/brown1.png";
-import brown2 from "@/assets/trashItems/brown/brown2.png";
 import organic_branch from "@/assets/trashItems/brown/organic_branch.png";
 import organic_bread from "@/assets/trashItems/brown/organic_bread.png";
 import organic_egg_shell from "@/assets/trashItems/brown/organic_egg_shell.png";
 import organic_flowers from "@/assets/trashItems/brown/organic_flowers.png";
 import organic_fructe_legume from "@/assets/trashItems/brown/organic_fructe_legume.png";
-import green1 from "@/assets/trashItems/green/green1.png";
-import green2 from "@/assets/trashItems/green/green2.png";
-import green3 from "@/assets/trashItems/green/green3.png";
 import sticla_borcan from "@/assets/trashItems/green/sticla_borcan.png";
 import sticla_cola from "@/assets/trashItems/green/sticla_cola.png";
 import sticla_pahar from "@/assets/trashItems/green/sticla_pahar.png";
@@ -44,13 +32,6 @@ import plastic_napolact_sana from "@/assets/trashItems/yellow/plastic_napolact_s
 import plastic_pahar_transparent from "@/assets/trashItems/yellow/plastic_pahar_transparent.png";
 import plastic_punga_ldpe from "@/assets/trashItems/yellow/plastic_punga_ldpe.png";
 import plastic_zuzu from "@/assets/trashItems/yellow/plastic_zuzu.png";
-import yellow1 from "@/assets/trashItems/yellow/yellow1.png";
-import yellow2 from "@/assets/trashItems/yellow/yellow2.png";
-import yellow3 from "@/assets/trashItems/yellow/yellow3.png";
-import yellow4 from "@/assets/trashItems/yellow/yellow4.png";
-import yellow5 from "@/assets/trashItems/yellow/yellow5.png";
-import yellow6 from "@/assets/trashItems/yellow/yellow6.png";
-
 export interface TrashItemImageProps {
   imageId: string;
   label: string;
@@ -77,24 +58,6 @@ const TrashItemImage: React.FC<TrashItemImageProps> = ({
 };
 
 const imageMapping: Record<string, StaticImageData> = {
-  black1,
-  black2,
-  black3,
-  blue1,
-  blue2,
-  blue3,
-  blue4,
-  brown1,
-  brown2,
-  green1,
-  green2,
-  green3,
-  yellow1,
-  yellow2,
-  yellow3,
-  yellow4,
-  yellow5,
-  yellow6,
   plastic_zuzu,
   plastic_napolact_lapte,
   plastic_napolact_sana,
