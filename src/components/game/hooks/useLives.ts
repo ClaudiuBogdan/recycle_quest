68afbc7b5792b5911976692669fae34312e7e32c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-10 14:01:42 +0200

    fix(lives): set min lives to 0

--- src/components/game/hooks/useLives.ts
+++ src/components/game/hooks/useLives.ts
@@ -4,7 +4,10 @@ export function useLives() {
   const totalLives = 3;
   const [lives, setLives] = useState(totalLives);
 
-  const removeLife = useCallback(() => setLives((lives) => lives - 1), []);
+  const removeLife = useCallback(
+    () => setLives((lives) => Math.max(lives - 1, 0)),
+    [],
+  );
   const addLife = useCallback(() => setLives((lives) => lives + 1), []);
 
   return { lives, totalLives, removeLife, addLife };
